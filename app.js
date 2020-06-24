dfa5a7c57c327df4a98e9167dc97ff1d595887a5
zainhashmi18 <zainhashmi275@gmail.com>
2020-06-25 00:49:57 +0500

    Final Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+function getNum(num){
+    var result=document.getElementById("res");
+    result.value+=num;
+    document.getElementById('res1').innerHTML ="Last Digit: "+result.value;
+}
+function clearResult(){
+    var result=document.getElementById("res");
+    result.value="";
+    document.getElementById('res1').innerHTML=""
+}
+function getResult(){
+    var result=document.getElementById("res");
+    result.value=eval(result.value);
+    console.log(result.value)
+}
+
