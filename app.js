2a648e82d8dbb0ea6a01d92473f19fa02bb0f35d
zainhashmi18 <zainhashmi275@gmail.com>
2020-06-26 21:41:15 +0500

    Final Commit

--- app.js
+++ app.js
@@ -13,4 +13,12 @@ function getResult(){
     result.value=eval(result.value);
     console.log(result.value)
 }
-
+function day() {
+    document.getElementById("main").style.backgroundColor = "white";
+    document.getElementById("main").style.borderColor = "white";
+    document.getElementByid("sayl").style.backgroundColor = "white";
+  }
+function night() {
+    document.getElementById("main").style.backgroundColor = "#21211F";
+    document.getElementById("main").style.borderColor = "#21211F"; 
+}
